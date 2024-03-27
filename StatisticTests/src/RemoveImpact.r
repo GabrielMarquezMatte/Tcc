8de6489e4a45cf52bbd305252fc30ca28c8f1959
Gabriel Matte <gabrielandremarquez.matte@gmail.com>
2024-03-27 02:40:43 -0300

    Refatora e simplifica código R para modelos GARCH e remove arquivo desnecessário
    - Simplifica a função `FindBestArchModel` removendo parâmetros não utilizados e linhas desnecessárias, focando em melhor legibilidade e eficiência.
    - Ajusta formatação do código em `garch_models.r` para manter consistência e facilitar a leitura.
    - Remove o arquivo `remove_market_impact.r`, uma vez que suas funcionalidades foram integradas em outros scripts ou se tornaram obsoletas.
    - Atualiza o método `DoSingleGarch` para simplificar sua implementação, removendo parâmetros não utilizados e melhorando a clareza do processo de estimação dos modelos GARCH.
    - Ajusta a função `FindBestDccModel` para incluir múltiplos modelos e distribuições, aumentando a flexibilidade na análise de séries temporais multivariadas.
    
    Essas mudanças visam otimizar o código de análise estatística, aumentar a manutenibilidade e preparar o ambiente para futuras expansões e melhorias.

---
+++ StatisticTests/src/RemoveImpact.r
@@ -0,0 +1,101 @@
+source("./src/libs/garch_models.r")
+QUERY_MARKET <- '
+SELECT "Date",
+        EXP(LN("Adjusted") - LAG(LN("Adjusted"), 1, LN("Adjusted")) OVER(PARTITION BY "TickerId"
+                                                                            ORDER BY "Date"))-1 AS "Return",
+        LN("Volume" * "Adjusted") AS "Volume"
+FROM "HistoricalDataYahoo"
+WHERE "Date" > $1
+AND "Adjusted" > 0
+'
+
+ALL_SECTORS_QUERY <- '
+SELECT
+    "HistoricalDataYahoo"."Date"
+    ,"Industries"."SectorId"
+    ,EXP(LN("HistoricalDataYahoo"."Adjusted") - LAG(LN("HistoricalDataYahoo"."Adjusted"), 1, LN("HistoricalDataYahoo"."Adjusted")) OVER(PARTITION BY "HistoricalDataYahoo"."TickerId" ORDER BY "HistoricalDataYahoo"."Date"))-1 AS "Return"
+    ,LN("HistoricalDataYahoo"."Volume") AS "Volume"
+FROM "HistoricalDataYahoo"
+INNER JOIN "Tickers" ON "HistoricalDataYahoo"."TickerId" = "Tickers"."Id"
+INNER JOIN "Companies" ON "Companies"."Id" = "Tickers"."CompanyId"
+INNER JOIN "CompanyIndustries" ON "CompanyIndustries"."CompanyId" = "Companies"."Id"
+INNER JOIN "Industries" ON "Industries"."Id" = "CompanyIndustries"."IndustryId"
+WHERE "Date" > $1
+AND "Adjusted" > 0
+AND "SectorId" = $2
+'
+
+VOLATILITY_MODELS <- c("eGARCH", "iGARCH", "gjrGARCH")
+DISTRIBUTIONS <- c("std", "sstd")
+
+DCC_MODEL <- c("DCC", "aDCC")
+DCC_DISTRIBUTION <- c("mvnorm", "mvt", "mvlaplace")
+
+CreateConnection <- function() {
+    return(odbc::dbConnect(RPostgres::Postgres(),
+        dbname = "stock", host = "localhost",
+        port = 5432, user = "postgres", password = "postgres"
+    ))
+}
+
+GetMarketValues <- function(conn, date) {
+    `%>%` <- magrittr::`%>%`
+    data <- odbc::dbGetQuery(conn, QUERY_MARKET, params = list(date)) %>%
+        dplyr::group_by(Date) %>%
+        dplyr::summarize(MarketReturn = log(1 + sum(Return * Volume) / sum(Volume)))
+    garch_model <- FindBestArchModel(data$MarketReturn, type_models = VOLATILITY_MODELS, dist_to_use = DISTRIBUTIONS)
+    data <- data %>%
+        dplyr::mutate(MarketVolatility = garch_model$fit_bic@fit$sigma)
+    result <- list(data = data, garch_model = garch_model$fit_bic, garch_spec = garch_model$ugspec_b)
+    return(result)
+}
+
+GetSectorValues <- function(conn, date, sector) {
+    return(odbc::dbGetQuery(conn, ALL_SECTORS_QUERY, params = list(date, sector)))
+}
+
+GetSectors <- function(conn) {
+    return(odbc::dbGetQuery(conn, 'SELECT * FROM "Sector" WHERE "Id" > 4'))
+}
+
+CalculateVolatilityForSector <- function(all_values, market_result) {
+    `%>%` <- magrittr::`%>%`
+    sector_df <- all_values %>%
+        dplyr::filter(abs(Return) < 0.5) %>%
+        dplyr::group_by(Date) %>%
+        dplyr::summarize(SectorReturn = log(1 + sum(Return * Volume) / sum(Volume)))
+    joined <- dplyr::inner_join(sector_df, market_result$data, by = "Date")
+    garch_model <- FindBestArchModel(joined$SectorReturn, type_models = VOLATILITY_MODELS, dist_to_use = DISTRIBUTIONS)
+    multispec <- rugarch::multispec(c(garch_model$ugspec_b, market_result$garch_spec))
+    only_returns <- joined %>% dplyr::select(SectorReturn, MarketReturn) %>% as.matrix()
+    multifit <- rugarch::multifit(multispec = multispec, data = only_returns)
+    dcc_model <- FindBestDccModel(only_returns, uspec = multispec,
+                                  type_models = DCC_MODEL,
+                                  dist_to_use = DCC_DISTRIBUTION, fit = multifit)
+    sector_volatility <- garch_model$fit_bic@fit$sigma
+    market_volatility <- joined$MarketVolatility
+    covariance <- rmgarch::rcov(dcc_model$fit_bic)
+    betas <- covariance[1, 2, ] / covariance[2, 2, ]
+    adjusted_volatility <- sqrt(sector_volatility^2 - (betas * market_volatility)^2)
+    only_returns <- joined %>%
+      dplyr::select(Date, SectorReturn, MarketReturn) %>%
+      dplyr::mutate(SectorVolatility = adjusted_volatility,
+                    NonAdjusted = sector_volatility,
+                    MarketVolatility = joined$MarketVolatility,
+                    MarketImpact = sqrt(betas^2*market_volatility^2))
+    return(only_returns)
+}
+
+main <- function() {
+    start_date <- as.Date("2017-01-01")
+    connection <- CreateConnection()
+    market_values <- GetMarketValues(connection, start_date)
+    sectors <- GetSectors(connection)
+    for(sector in sectors$Id) {
+        all_values <- GetSectorValues(connection, start_date, sector)
+        only_returns <- CalculateVolatilityForSector(all_values, market_values)
+    }
+    odbc::dbDisconnect(connection)
+}
+
+main()
--- StatisticTests/src/libs/garch_models.r
+++ StatisticTests/src/libs/garch_models.r
@@ -1,37 +1,30 @@
 FindBestArchModel <- function(x, type_models = c(
-                                     "eGARCH", "sGARCH",
-                                     "csGARCH", "iGARCH",
-                                     "gjrGARCH", "apARCH"
-                                 ),
-                                 dist_to_use = c(
-                                     "std", "sstd", "jsu", "nig",
-                                     "norm", "snorm", "ged", "sged"
-                                 ),
-                                 max_AR = 1, max_MA = 1, max_ARCH = 2, max_GARCH = 2,
-                                 min_ARCH = 1, min_GARCH = 1, min_AR = 0, min_MA = 0,
-                                 external.regressors = NULL, include.mean = c(T, F),
-                                 arfima = F, simplified = T) {
+                                  "eGARCH", "sGARCH",
+                                  "csGARCH", "iGARCH",
+                                  "gjrGARCH", "apARCH"
+                              ),
+                              dist_to_use = c(
+                                  "std", "sstd", "jsu", "nig",
+                                  "norm", "snorm", "ged", "sged"
+                              ),
+                              max_AR = 1, max_MA = 1, max_ARCH = 2, max_GARCH = 2,
+                              min_ARCH = 1, min_GARCH = 1, min_AR = 0, min_MA = 0,
+                              simplified = T) {
     `%>%` <- magrittr::`%>%`
     if (simplified) {
         message("Distribution fitting")
         a <- lapply(dist_to_use, rugarch::fitdist, x = x)
         b <- sapply(a, \(x)min(x$values))
         dist_to_use <- dist_to_use[which.min(b)]
-        expanded <- tidyr::expand_grid(include.mean,
-            criterion = c("AIC", "BIC"),
-            arfima
-        )
+        expanded <- tidyr::expand_grid(criterion = c("AIC", "BIC"))
         message("ARFIMA model")
         auto <- purrr::pmap(
             list(
-                criterion = expanded$criterion,
-                include.mean = expanded$include.mean,
-                arfima = expanded$arfima
+                criterion = expanded$criterion
             ),
             rugarch::autoarfima,
             data = x, ar.max = max_AR, ma.max = max_MA,
-            method = "partial", external.regressors = external.regressors,
-            distribution.model = dist_to_use
+            method = "partial", distribution.model = dist_to_use
         )
         auto_m <- lapply(auto, \(x)as.data.frame(x$rank.matrix)) %>%
             dplyr::bind_rows() %>%
@@ -39,18 +32,15 @@ FindBestArchModel <- function(x, type_models = c(
             dplyr::mutate(BIC = ifelse(is.na(BIC), 0, BIC), AIC = ifelse(is.na(AIC), 0, AIC))
         aic <- auto_m[which.min(auto_m$AIC), ]
         bic <- auto_m[which.min(auto_m$BIC), ]
-        include.mean <- unique(c(aic$Mean, bic$Mean) == 1)
         max_AR <- max(aic$AR, bic$AR)
         max_MA <- max(aic$MA, bic$MA)
         min_AR <- min(aic$AR, bic$AR)
         min_MA <- min(aic$MA, bic$MA)
-        arfima <- unique(c(aic$ARFIMA, bic$ARFIMA)) == 1
         type_mod <- tidyr::expand_grid(
             type_models = type_models,
             dist_to_use = dist_to_use,
             arma_lag = min_AR:max_AR, ma_lag = min_MA:max_MA,
-            arch_lag = 1, garch_lag = 1,
-            include.mean, arfima
+            arch_lag = 1, garch_lag = 1
         )
         message("Best GARCH model")
         type_mods <- purrr::pmap(
@@ -60,12 +50,10 @@ FindBestArchModel <- function(x, type_models = c(
                 lag_ar = type_mod$arma_lag,
                 lag_ma = type_mod$ma_lag,
                 lag_arch = type_mod$arch_lag,
-                lag_garch = type_mod$garch_lag,
-                include.mean = type_mod$include.mean,
-                arfima = type_mod$arfima
+                lag_garch = type_mod$garch_lag
             ),
             DoSingleGarch,
-            x = x, external.regressors = external.regressors
+            x = x
         )
         type_mods <- lapply(type_mods, function(x) x$table) %>% dplyr::bind_rows()
         bic <- type_mods[which.min(type_mods$BIC), ]
@@ -77,8 +65,6 @@ FindBestArchModel <- function(x, type_models = c(
         min_AR <- min(aic$lag_ar, bic$lag_ar)
         max_AR <- max(aic$lag_ar, bic$lag_ar)
         max_MA <- max(aic$lag_ma, bic$lag_ma)
-        include.mean <- unique(c(aic$include.mean, bic$include.mean))
-        arfima <- unique(c(aic$arfima, bic$arfima))
     }
     df_grid <- tidyr::expand_grid(
         type_models = type_models,
@@ -86,8 +72,7 @@ FindBestArchModel <- function(x, type_models = c(
         arma_lag = min_AR:max_AR,
         ma_lag = min_MA:max_MA,
         arch_lag = min_ARCH:max_ARCH,
-        garch_lag = min_GARCH:max_GARCH,
-        include.mean, arfima
+        garch_lag = min_GARCH:max_GARCH
     )
     message("Best model")
     l_out <- purrr::pmap(
@@ -97,11 +82,9 @@ FindBestArchModel <- function(x, type_models = c(
             lag_ar = df_grid$arma_lag,
             lag_ma = df_grid$ma_lag,
             lag_arch = df_grid$arch_lag,
-            lag_garch = df_grid$garch_lag,
-            include.mean = df_grid$include.mean,
-            arfima = df_grid$arfima
+            lag_garch = df_grid$garch_lag
         ),
-        DoSingleGarch, x = x, external.regressors = external.regressors
+        DoSingleGarch, x = x
     )
     tab_out <- lapply(l_out, function(x) x$table) %>%
         dplyr::bind_rows()
@@ -126,8 +109,7 @@ FindBestArchModel <- function(x, type_models = c(
     return(l_out)
 }
 DoSingleGarch <- function(x, type_model, type_dist, lag_ar,
-                            lag_ma, lag_arch, lag_garch, include.mean = T,
-                            arfima = F, external.regressors = NULL) {
+                          lag_ma, lag_arch, lag_garch) {
     spec <- NA
     message("Estimating ARMA(", lag_ar, ",", lag_ma, ")-",
         type_model, "(", lag_arch, ",", lag_garch, ")",
@@ -140,10 +122,7 @@ DoSingleGarch <- function(x, type_model, type_dist, lag_ar,
             garchOrder = c(lag_arch, lag_garch)
         ),
         mean.model = list(
-            armaOrder = c(lag_ar, lag_ma),
-            external.regressors = external.regressors,
-            arfima = arfima,
-            include.mean = include.mean
+            armaOrder = c(lag_ar, lag_ma)
         ),
         distribution = type_dist
     )
@@ -165,7 +144,7 @@ DoSingleGarch <- function(x, type_model, type_dist, lag_ar,
         BIC <- NA
     }
 
-    est_tab <- dplyr::tibble(lag_ar, lag_ma, lag_arch, lag_garch, include.mean, arfima,
+    est_tab <- dplyr::tibble(lag_ar, lag_ma, lag_arch, lag_garch,
         AIC = AIC, BIC = BIC, type_model = type_model,
         type_dist, model_name = paste0(
             "ARMA(", lag_ar, ",", lag_ma, ")+",
@@ -177,12 +156,12 @@ DoSingleGarch <- function(x, type_model, type_dist, lag_ar,
     return(lista)
 }
 DoSingleDcc <- function(x, uspec,
-                          type_model = "DCC",
-                          type_dist = "mvnorm",
-                          order1,
-                          order2,
-                          fit.control,
-                          fit = NULL) {
+                        type_model,
+                        type_dist,
+                        order1,
+                        order2,
+                        fit.control,
+                        fit = NULL) {
     spec <- rmgarch::dccspec(uspec,
         dccOrder = c(order1, order2),
         model = type_model, distribution = type_dist
@@ -195,10 +174,11 @@ DoSingleDcc <- function(x, uspec,
         my_rugarch <- list()
         my_rugarch <- rmgarch::dccfit(spec = spec, data = x, fit = fit, fit.control = fit.control)
     })
-    if (!is.null(coef(my_rugarch))) {
+    if (!is.null(rugarch::coef(my_rugarch))) {
         message("\tDone")
-        AIC <- rugarch::infocriteria(my_rugarch)[1]
-        BIC <- rugarch::infocriteria(my_rugarch)[2]
+        info <- rugarch::infocriteria(my_rugarch)
+        AIC <- info[1]
+        BIC <- info[2]
     } else {
         message("\tEstimation failed..")
         AIC <- NA
@@ -218,16 +198,16 @@ DoSingleDcc <- function(x, uspec,
     )
     return(lista)
 }
-FindBestDccModel <- function(x, uspec, type_models = "DCC",
-                                dist_to_use = "mvnorm",
-                                max_order1 = 2, max_order2 = 2,
-                                min_order1 = 1, min_order2 = 1,
-                                fit.control = list(
-                                    eval.se = F,
-                                    stationarity = T,
-                                    scale = F
-                                ),
-                                fit = NULL) {
+FindBestDccModel <- function(x, uspec, type_models = c("DCC", "aDCC"),
+                             dist_to_use = c("mvnorm", "mvt"),
+                             max_order1 = 2, max_order2 = 2,
+                             min_order1 = 1, min_order2 = 1,
+                             fit.control = list(
+                                 eval.se = F,
+                                 stationarity = T,
+                                 scale = F
+                             ),
+                             fit = NULL) {
     `%>%` <- magrittr::`%>%`
     df_grid <- tidyr::expand_grid(
         type_models = type_models,
